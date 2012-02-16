cbcfc658127ba47364f39860b9886ecf8ed2b762
Max Penet <max.penet@gmail.com>
2012-02-16 22:30:47 +0100

    add clojars prefix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-oauth "1.3.1-SNAPSHOT"
+(defproject org.clojars.mpenet/clj-oauth "1.3.1-SNAPSHOT"
   :description "OAuth support for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [aleph "0.2.1-alpha2-SNAPSHOT"]])
